f955cc68ec6979d9228551483c39c2e71bdd9b80
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2022-10-28 06:57:58 -0300

    finished

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -34,7 +34,7 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
     }, []);
 
     async function createTransaction(transactionInput: TransactionInput){
-        const response = await api.post('/transactions', {
+        const response = await api.post('transactions', {
             ...transactionInput,
             createdAt: new Date(),
         })
