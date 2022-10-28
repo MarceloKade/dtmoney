7f961aa52c0a3c43ae4833610181f29749a611b9
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2022-10-28 05:07:36 -0300

    project finish

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,67 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { useTransactions } from '../../hooks/useTransactions';
+import { Container } from "./styles";
+
+export function Summary(){
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amout;
+            acc.total += transaction.amout;
+        } else {
+            acc.withdraws += transaction.amout;
+            acc.total -= transaction.amout;
+        }
+        return acc;
+    },   {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    })
+    
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(summary.deposits)}
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>
+                    -
+                    {new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(summary.withdraws)}
+                </strong>
+            </div>
+            <div className='highlight-background'>
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL'
+                        }).format(summary.total)}
+                </strong>
+            </div>
+        </Container>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createServer, Model} from 'miragejs'
+import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amout: 6000,
+          createdAt: new Date('2021-02-12 9:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amout: 1100,
+          createdAt: new Date('2021-02-14 11:00:00')
+        }
+      ],
+    })
+  },
+
+  routes(this) {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+    
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  },
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +54,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
